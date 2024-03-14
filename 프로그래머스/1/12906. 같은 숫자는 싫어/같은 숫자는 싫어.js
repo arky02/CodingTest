bdac608cef7798ec1c58707f4e99bf469a05d921
Yeon Kim <46954114+arky02@users.noreply.github.com>
2024-03-15 01:24:45 +0900

    Update 같은 숫자는 싫어.js

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,13 +1,10 @@
 function solution(arr)
 {
-    var answer = [];
-    
+    let answer = [];
+
     for(num of arr){
         if (answer.length === 0 || answer[answer.length-1] !== num) answer.push(num);
     }
-
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    console.log('Hello Javascript')
     
     return answer;
 }
