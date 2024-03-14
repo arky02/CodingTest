88d4f4b5164c7c2e78f60b87c693b24de391b3ce
Yeon Kim <46954114+arky02@users.noreply.github.com>
2024-03-15 01:25:02 +0900

    Update 행렬의 덧셈.js

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,10 +1,9 @@
 function solution(arr1, arr2) {
-    var answer = new Array(arr1.length);
+    let answer = new Array(arr1.length);
     
     for(let i =0; i< arr1.length; i++){
         answer[i] = new Array(arr1[0].length);
         for(let j = 0; j< arr1[0].length; j++){
-            answer[i][j] = 0;
             answer[i][j] = arr1[i][j] + arr2[i][j] 
         }
     }
