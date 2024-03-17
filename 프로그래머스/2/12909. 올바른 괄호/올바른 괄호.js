747c9eeffa5275a511a251369473199372a8019b
Yeon Kim <46954114+arky02@users.noreply.github.com>
2024-03-18 03:09:07 +0900

    [level 2] Title: 올바른 괄호, Time: 26.26 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,11 @@
+function solution(s){
+    let count = 0;
+    
+    for(let i = 0; i< s.length; i++){
+        if(count<0) return false;
+        if (s[i] === "(") ++count;
+        else --count;
+    }
+    
+    return (count === 0) ? true : false;
+}
