08f257c0a40e6152ffde2041ee79e5b40db0c1ea
Yeon Kim <46954114+arky02@users.noreply.github.com>
2024-03-30 14:19:39 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -0,0 +1,10 @@
+function solution(my_string) {
+    const arr = my_string.split(' ');
+    let result = +arr[0];
+    
+    for(let i = 1; i< Math.ceil(arr.length/2); i++){
+        arr[2*i-1] === '+' ? result += + arr[2*i] : result -= +arr[2*i]
+    }
+    
+    return result;
+}
