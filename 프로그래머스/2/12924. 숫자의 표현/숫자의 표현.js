59175cfd1a7cf5fbcb8b16f19a0c3950e9375c4b
Yeon Kim <46954114+arky02@users.noreply.github.com>
2024-04-08 05:50:00 +0900

    [level 2] Title: 숫자의 표현, Time: 1.50 ms, Memory: 36.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    let answer = 0;
+    for(let i = n; i>0; i--){
+        let res = 0;
+        for(let j = i; j>0; j--){
+            res +=j;
+            res === n && answer++;
+            if(res>n)break;
+        }
+    }
+    return answer;
+}
