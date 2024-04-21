ae7ca6484ea8b2232415c0adbd61660ccdc8b772
Yeon Kim <46954114+arky02@users.noreply.github.com>
2024-04-22 01:49:52 +0900

    [level 0] Title: 코드 처리하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -0,0 +1,9 @@
+function solution(code) {
+    let answer = '';
+    let mode = false;
+    code.split('').map((el,idx)=> {
+        if(+el) mode = !mode
+        else if((mode && idx%2) || (!mode && idx%2 === 0)) answer += el
+    })
+    return answer.length ? answer : 'EMPTY';
+}
