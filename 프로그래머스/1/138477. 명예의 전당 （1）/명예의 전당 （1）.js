48dbf2000abf5c08ec4b1a3c242c55e3e3a586fb
Yeon Kim <46954114+arky02@users.noreply.github.com>
2024-04-29 02:31:39 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -0,0 +1,10 @@
+function solution(k, score) {
+    let ans = [];
+    return score.map((el)=> {
+        ans = ans.sort((a,b)=> b-a)
+        if(ans.length < k) ans.push(+(el))
+        else if(ans.length >= k && ans[k-1]<= +(el)) {ans.pop(); ans.push(el)}
+        return Math.min(...ans);
+    })
+    
+}
