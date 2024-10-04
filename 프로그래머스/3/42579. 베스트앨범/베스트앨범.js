52d3f3986932f7636416d770dee4a9c512bccf60
Yeon Kim <46954114+arky02@users.noreply.github.com>
2024-10-05 02:14:36 +0900

    Update 베스트앨범.js

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,28 +1,17 @@
-function solution(genres, plays) {  
-    
-    let genreRank = [];
-    
+function solution(genres, plays) {      
     const map = new Map();
-    for(let i = 0; i<genres.length ; i++){
-        if(!map.has(genres[i])){
-            map.set(genres[i], [[plays[i],i]])
-        }else{
-            map.get(genres[i]).push([plays[i],i])
-            map.get(genres[i]).sort((a,b)=>b[0]-a[0])
-        }
+    
+    for(let i = 0; i<genres.length ; i++){     
+        const newVal = [plays[i],i];
+        const currGenre = genres[i];
         
+        !map.has(currGenre) ? map.set(currGenre, [newVal]) : map.get(currGenre).push(newVal);
+        map.get(currGenre).sort((a,b)=>b[0]-a[0])  ;     
     }
-    console.log(map)
     
-    let genresArray = [...new Set(genres)];
-    const sorted = genresArray.sort((a,b)=> map.get(b).reduce((acc,curr)=> acc+curr[0],0) - map.get(a).reduce((acc,curr)=> acc+curr[0],0));
+    let genreArray = [...new Set(genres)];
     
+    const sortedGenreArr = genreArray.sort((a,b)=> map.get(b).reduce((acc,curr)=> acc+curr[0],0) - map.get(a).reduce((acc,curr)=> acc+curr[0],0));
     
-    
-    const ans = sorted.map((el)=>{
-        const temp = map.get(el).slice(0,2).map((i)=> i[1])
-        return temp
-    })
-    return ans.flat()
-
+   return sortedGenreArr.map((el)=> map.get(el).slice(0,2).map((i)=> i[1])).flat()
 }
